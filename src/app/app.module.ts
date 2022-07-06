a831e2ee99da4269d3d1da19a15dc38056636f74
Bruno <bpayet.gsm@free.fr>
2022-07-06 16:42:10 +0200

    memory card final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { Exo1Component } from './pages/exo1/exo1.component';
+import { Exo3Component } from './pages/exo3/exo3.component';
+import { Exo4Component } from './pages/exo4/exo4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    Exo1Component,
+    Exo3Component,
+    Exo4Component,
   ],
   imports: [
     BrowserModule,
